2adc73ccec8ce2344799f01c00806afa4a35b463
leekm0912 <skatjrdndqo@naver.com>
2020-12-02 09:18:13 +0900

    읽기, 쓰기 permission 검사 추가.

--- app/src/main/java/net/ddns/leekm/yonam_market/SellPage.java
+++ app/src/main/java/net/ddns/leekm/yonam_market/SellPage.java
@@ -1,10 +1,15 @@
 package net.ddns.leekm.yonam_market;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -24,6 +29,7 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -48,7 +54,8 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Pattern;
 
-public class SellPage extends AppCompatActivity {
+public class SellPage extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback{
+
     private final int GET_GALLERY_IMAGE = 200;
     TextView t;
     EditText title;
@@ -60,6 +67,10 @@ public class SellPage extends AppCompatActivity {
     ImageView imgVwSelected;
     File tempSelectFile;
     byte[] fileArray;
+    // 앱을 실행하기 위해 필요한 퍼미션을 정의합니다.
+    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+    // 퍼미선 작업 결과 코드
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -160,11 +171,79 @@ public class SellPage extends AppCompatActivity {
     }
 
     public void imgUpload(View v){
-        Intent intent = new Intent();
-        // intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, 1);
+        // 1. 외부 저장소 퍼미션을 가지고 있는지 체크합니다.
+        int writeExternalStoragePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readExternalStoragePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        //2-1 퍼미션 권한이 있다면 바로 동작
+        if ( writeExternalStoragePermission == PackageManager.PERMISSION_GRANTED &&
+                readExternalStoragePermission == PackageManager.PERMISSION_GRANTED){
+            Intent intent = new Intent();
+            // intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(intent, 1);
+        }else{ //2-2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
+            || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])){
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Toast.makeText(this,"외부 저장소 접근 권한이 필요합니다.",Toast.LENGTH_SHORT).show();
+                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions( this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+            }else{
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions( this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+            }
+        }
+
+    }
+
+    // 퍼미션 요청(ActivityCompat.requestPermissions)에 대한 결과를 리턴받으려면
+    // OnRequestPermissionsResultCallback 콜백의 onRequestPermissionsResult 메소드를 구현해줘야 합니다.
+    // onRequestPermissionsResult에서 수신된 결과에서 ActivityCompat.requestPermissions를 사용한 퍼미션 요청을 구별하기 위해 사용됩니다.
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
+        // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
+        if (requestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+
+            // 모든 퍼미션을 허용했는지 체크합니다.
+            boolean check_result = true;
+            
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+            // 모든 퍼미션을 허용했다면 작업을 시작합니다.
+            if (check_result) {
+                Intent intent = new Intent();
+                // intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+            } else {// 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
+                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+                    // 사용자가 거부만 선택한 경우에는 앱을 다시 실행하여 허용을 선택하면 앱을 사용할 수 있습니다.
+                    Toast.makeText(this, "퍼미션이 거부되었습니다. 앱을 사용하기 위해서 퍼미션을 허용해주세요. ",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+
+                } else {
+                    // “다시 묻지 않음”을 사용자가 체크하고 거부를 선택한 경우에는 설정(앱 정보)에서 퍼미션을 허용해야 앱을 사용할 수 있습니다.
+                    Toast.makeText(this, "퍼미션이 거부되었습니다. 앱을 사용하기 위해서 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        }
     }
 
 
